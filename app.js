bff01d8614a042c06b7fcc115de041de5418a28d
jazperYdio <jazper.ydio0224@gmail.com>
2021-11-17 09:46:20 +0800

    Modified app.js port connection

--- app.js
+++ app.js
@@ -97,6 +97,8 @@ app.post("/delete", (req, res) => {
     });
 });
 
-app.listen(3000 || process.env.PORT, function() {
-    console.log("Server started on port 3000");
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
